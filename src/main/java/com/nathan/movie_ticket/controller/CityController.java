1ddb0576395fc0505f7cdffe9ddd3ee16c3fd4d5
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-05-29 11:53:03 +0700

    feat(city): add get list city API

---
+++ src/main/java/com/nathan/movie_ticket/controller/CityController.java
@@ -0,0 +1,28 @@
+package com.nathan.movie_ticket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nathan.movie_ticket.dto.response.ListCityResDto;
+import com.nathan.movie_ticket.dto.response.PageResDto;
+import com.nathan.movie_ticket.service.CityService;
+
+@RestController
+@RequestMapping("v1/city")
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public PageResDto<ListCityResDto> getListCity(
+            @RequestParam(name = "city_name", required = false) String cityName,
+            @RequestParam(name = "page_number", required = false, defaultValue = "0") int pageNumber,
+            @RequestParam(name = "page_size", required = false, defaultValue = "10") int pageSize) {
+        return cityService.getListCity(pageNumber, pageSize, cityName);
+    }
+
+}
---
+++ src/main/java/com/nathan/movie_ticket/dto/response/ListCityResDto.java
@@ -0,0 +1,5 @@
+package com.nathan.movie_ticket.dto.response;
+
+public record ListCityResDto(String name, Long id) {
+
+}
